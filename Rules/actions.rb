780243cb76cf4ab462db58c67b20aa3baa940b7b
Gilles Leblanc <gilles.leblanc@gmail.com>
2013-03-29 20:24:59 -0400

    Corrected syntax error.

--- Rules/actions.rb
+++ Rules/actions.rb
@@ -1,5 +1,5 @@
 module Actions
-	attack(target)
+	def attack(target)
 		if (1..10).to_a.shuffle.first + @accuracy / 4 >= target.ac
 			broadcast "#{@name} hits #{target.name}."
 			target.take_damage(@paper_doll.weapon.hit)
