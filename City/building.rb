3e4d38d7898a42bf3505cd8f3ddee60a93a1f970
Gilles Leblanc <gilles.leblanc@gmail.com>
2013-08-05 21:57:00 -0400

    Did some refactorings following running Rubocop on building.rb

--- City/building.rb
+++ City/building.rb
@@ -1,3 +1,6 @@
+# The physical representation of a building for a city.
+# This class can create a new building but will not create
+# it's events/content.
 class Building
   attr_reader :bounding_box_x, :bounding_box_y
   attr_accessor :bitmap
@@ -8,6 +11,22 @@ class Building
 
     @bitmap = Hash.new(:open)
 
+    place_blocks
+    place_door(facing) if @bitmap.any? { |key, value| value == :wall }
+  end
+
+  def draw_on(city, coordinates)
+    for x in 0..@bounding_box_x
+      for y in 0..@bounding_box_y
+        city.tile_at(x + coordinates[0], y + coordinates[1])
+          .type = @bitmap[[x, y]]
+      end
+    end
+  end
+
+  private
+
+  def place_blocks
     if [true, false].shuffle.first
       paint_block_1
     end
@@ -27,19 +46,7 @@ class Building
     if [true, false, false, false, false].shuffle.first
       remove_block
     end
-
-    place_door(facing) if @bitmap.any? { |key, value| value == :wall }
   end
-
-  def draw_on(city, coordinates)
-    for x in 0..@bounding_box_x
-      for y in 0..@bounding_box_y
-        city.tile_at(x + coordinates[0], y + coordinates[1]).type = @bitmap[[x, y]]
-      end
-    end
-  end
-
-  private
 
   def paint_block_1
     @bitmap[[1, 1]] = :wall
@@ -75,25 +82,31 @@ class Building
   end
 
   def place_door(facing)
+    key = @bitmap.select do |k, v|
+      v == :wall && k[k_value(facing)] == max_value(facing)
+    end.keys.shuffle.first
+
+    @bitmap[key] = :door
+  end
+
+  def max_value(facing)
     case facing
       when :north
         max_value = @bitmap.select { |k, v| v == :wall }.keys.min { |a, b| a[1] <=> b[1] }[1]
-        k_value = 1
       when :south
         max_value = @bitmap.select { |k, v| v == :wall }.keys.max { |a, b| a[1] <=> b[1] }[1]
-        k_value = 1
       when :east
         max_value = @bitmap.select { |k, v| v == :wall }.keys.max { |a, b| a[0] <=> b[0] }[0]
-        k_value = 0
       when :west
         max_value = @bitmap.select { |k, v| v == :wall }.keys.min { |a, b| a[0] <=> b[0] }[0]
-        k_value = 0
       else
         max_value = @bitmap.select { |k, v| v == :wall }.keys.min { |a, b| a[1] <=> b[1] }[1]
-        k_value = 1
     end
+    max_value
+  end
 
-    key = @bitmap.select { |k, v| v == :wall && k[k_value] == max_value }.keys.shuffle.first
-    @bitmap[key] = :door
+  def k_value(facing)
+    return 1 if facing == :north || facing == :south
+    0
   end
 end
