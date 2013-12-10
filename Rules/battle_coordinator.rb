25eb9c43b495c45d0c99c08e949b0f709a4c22a5
Gilles Leblanc <gilles.leblanc@gmail.com>
2013-12-09 21:08:47 -0500

    Split battle coordinator code and it's test code
    Fixes #11

--- Rules/battle_coordinator.rb
+++ Rules/battle_coordinator.rb
@@ -1,13 +1,4 @@
-require_relative 'items/shields'
-require_relative 'items/armors'
-require_relative 'items/weapons'
-require_relative 'mobs/goblin'
-require_relative 'classes/knight'
-require_relative 'classes/sorcerer'
-require_relative 'stats'
 require_relative '../broadcast'
-require_relative 'party'
-require_relative 'spells/spells'
 
 # Coordinates battles between the party (the characters)
 # and a group of monsters
@@ -70,47 +61,3 @@ class BattleCoordinator
     true
   end
 end
-
-padded_armor = PaddedArmor.new
-shield = LargeShield.new
-long_sword = LongSword.new
-dagger = LongDagger.new
-
-stats = Stats.new
-stats.might, stats.accuracy, stats.endurance = 20, 20, 20
-stats.intellect, stats.personality = 20, 20
-stats.speed, stats.luck = 20, 20
-
-felgar = Knight.new('Sir Felgar', stats)
-felgar.paper_doll.equip(shield)
-felgar.paper_doll.equip(padded_armor)
-felgar.paper_doll.equip(long_sword)
-
-cassandra = Sorcerer.new('Casssandra', stats)
-cassandra.paper_doll.equip(padded_armor)
-cassandra.paper_doll.equip(dagger)
-cassandra.spells[1].push(FlameArrow.new)
-
-party = Party.new
-party.members.push(felgar)
-party.members.push(cassandra)
-
-random_number_generator = Random.new
-
-enemies = []
-enemies.push(Goblin.new(random_number_generator))
-enemies.push(Goblin.new(random_number_generator))
-
-battle_coordinator = BattleCoordinator.new(random_number_generator)
-
-puts 'You are : '
-party.members.each { |c| puts c.class.name + ' ' + c.name }
-puts ' --- '
-puts
-
-puts 'You face : '
-enemies.each { |e| puts e.name }
-puts ' --- '
-puts
-
-battle_coordinator.run_battle(party, enemies)
